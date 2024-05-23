8856a396b900b8f65636b5a0583ee7b84bbffa35
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-23 23:56:09 +0300

    added the node.js to enable using react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>My name is Mercy Ogalo</p>
+     <h1> Frontend developer</h1>
+    </div>
+  );
+}
+
+export default App;
