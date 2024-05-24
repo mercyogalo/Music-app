35f708deb62818cf7a8fc2cd34e173f9cd61e340
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2024-05-24 22:51:30 +0300

    added  node.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <p>My name is Mercy Ogalo</p>
-     <h1> Frontend developer</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
